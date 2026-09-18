701dcff8f96e1ae96721f5616bebadcecefa30ba
Hassan <243121482+hassan146-byte@users.noreply.github.com>
2026-09-18 23:07:57 +0100

    Added another function which plays the game
    Version before final version that will move the playRound function within
    the playGame function to be played 5 times.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -19,9 +19,6 @@ function getComputerChoice() {
     }
 }
 
-let result = getComputerChoice();
-console.log(result);
-
 /* getHumanChoice 
 Prompt user input
 Ask "select rock, paper, or scissors"
@@ -33,11 +30,55 @@ function getHumanChoice() {
     return choice;
 }
 
-let ask = getHumanChoice();
-console.log(ask);
-
 const humanScore = 0;
 const computerScore = 0;
 
-/* playRound 
 
+/* playRound 
+function playRound(humanChoice, computerChoice)
+make capitalisation not matter with return humanChoice.toLowerCase();
+let winner = humanChoice + " & " + computerChoice
+capture all losing scenarios 
+if winner = "rock & paper" || winner = "paper & scissors" || winner = "scissors & rock" 
+return "You lose! " + computerChoice + " beats " + humanChoice  
+repeat else if statements for winning game scenarios
+else 
+    return "It's a tie!"
+console.log(winner)
+*/
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice.toLowerCase();
+    console.log(computerChoice)
+    let winner = humanChoice + " & " + computerChoice;
+    
+    if (winner === "rock & paper" || winner === "paper & scissors" || winner === "scissors & rock") { 
+        console.log("You lose! " + computerChoice + " beats " + humanChoice);
+        computerScore++;
+    }        
+    else if (winner === "paper & rock" || winner === "scissors & paper" || winner === "rock & scissors") {
+        console.log("You Win! " + humanChoice + " beats " + computerChoice);
+        humanScore++;  
+    }
+    else {
+        console.log("It's a tie!");
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+/* playGame
+humanScore
+computerScore
+let n = 0
+while (n < 6) 
+n++
+playRound(getHumanChoice, getComputerchoice)
+*/
+
+function playGame() {
+    
+}
